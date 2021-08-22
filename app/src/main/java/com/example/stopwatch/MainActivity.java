9e06843217f1efe69fac8805a71091d2a3e5c75a
AdityaApte145 <adityaapte2014@gmail.com>
2021-08-22 16:08:40 +0530

    Stopwatch Project

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.SystemClock;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start, pause, stop;
+
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        start = findViewById(R.id.start);
+        pause = findViewById(R.id.pause);
+        stop = findViewById(R.id.stop);
+
+        chronometer.setFormat("%s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!running) {
+                    chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                    chronometer.start();
+                    running = true;
+                }
+                Toast.makeText(MainActivity.this, "Started", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (running) {
+                    chronometer.stop();
+                    pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    running = false;
+                }
+                Toast.makeText(MainActivity.this, "Paused", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                pauseOffset = 0;
+                Toast.makeText(MainActivity.this, "Stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
